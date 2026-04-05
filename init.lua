7772be282f324bcb6d2683e0c0d96a5b7e5e413d
Oleg Kostyuk <cub.uanic@gmail.com>
2026-04-05 15:12:12 +0200

    update

--- init.lua
+++ init.lua
@@ -87,9 +87,6 @@ Pre-requirements:
 P.S. You can delete this when you're done too. It's your config now! :)
 --]]
 
----@diagnostic disable-next-line: undefined-global
-local vim = vim
-
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
