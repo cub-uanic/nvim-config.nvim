2ca18fc67807b90bbeb6121e071b3c2822366598
Oleg Kostyuk <cub.uanic@gmail.com>
2026-05-20 22:57:02 +0200

    added AI: opencode

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -50,6 +50,37 @@ return {
     },
   },
 
+  {
+    "nickjvandyke/opencode.nvim",
+    version = "*", -- Latest stable release
+    dependencies = {
+      {
+        "folke/snacks.nvim",
+        optional = true,
+        opts = {
+          input = {}, -- Enhances `ask()`
+          picker = { -- Enhances `select()`
+            actions = { opencode_send = function(...) return require("opencode").snacks_picker_send(...) end },
+            win = { input = { keys = { ["<A-a>"] = { "opencode_send", mode = { "n", "i" } } } } },
+          },
+        },
+      },
+    },
+    config = function()
+      vim.o.autoread = true
+
+      vim.keymap.set({ "n", "x" }, "<C-.>", function() require("opencode").ask("@this: ", { submit = true }) end, { desc = "Ask opencode…" })
+      vim.keymap.set({ "n", "x" }, "<C-,>", function() require("opencode").select() end, { desc = "Select opencode…" })
+      vim.keymap.set({ "n", "t" }, "<C-\\>", function() require("opencode").toggle() end, { desc = "Toggle opencode" })
+
+      vim.keymap.set({ "v", "x" }, "go", function() return require("opencode").operator("@this ") end, { desc = "Add range to opencode", expr = true })
+      vim.keymap.set("n", "go", function() return require("opencode").operator("@this ") .. "_" end, { desc = "Add line to opencode", expr = true })
+
+      vim.keymap.set("n", "<S-C-u>", function() require("opencode").command("session.half.page.up") end, { desc = "Scroll opencode up" })
+      vim.keymap.set("n", "<S-C-d>", function() require("opencode").command("session.half.page.down") end, { desc = "Scroll opencode down" })
+    end,
+  },
+
   -- most preferred colorschemes
   { "catppuccin/nvim" },
   { "folke/tokyonight.nvim" },
